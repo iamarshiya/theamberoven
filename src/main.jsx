929dcb7aa301a60fdad23acea0c336525aaac64b
iamarshiya <202301070209@mitaoe.ac.in>
2025-12-27 00:26:38 +0530

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { Analytics } from "@vercel/analytics/next";
 
-createRoot(document.getElementById("root")).render(
-  <>
-    <App />
-    <Analytics />
-  </>
-);
+
+createRoot(document.getElementById("root")).render(<App />);
